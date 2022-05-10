1818b99cd91b556bc911a9487bb1e508337c9b0e
Prabin Parajuli <prabin.b9@gmail.com>
2022-05-10 11:43:07 +0200

    Securing the Smart Contract
    Only owner is able to deploy the smart contract

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -2,11 +2,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleWallet{
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You are not allowed");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
 
     fallback () external payable {
-
+        
     }
 }
